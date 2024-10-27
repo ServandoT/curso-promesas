7f799c2170b7cc07505981b791da9c8a705c6825
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-10-27 14:05:12 +0100

    Añadido comentario a ejemplo3

--- ejemplo3/promesas.js
+++ ejemplo3/promesas.js
@@ -1,21 +1,25 @@
-// Petición con JQuery
-$( document ).ready(function() {
-    console.log('Antes de la petición');
-    $.get('https://reqres.in/api/users').then( 
-        (respuesta) => {
-            console.log('dentro del then');
-            console.log(respuesta);
-        });
-    console.log('después de la petición');
-});
-
-// Petición con API Fetch
+// Petición con JQuery, no nativa de JS
 // $( document ).ready(function() {
 //     console.log('Antes de la petición');
-//     fetch('https://reqres.in/api/users').then( 
+
+//     // Hago una petición de tipo get a una y que devuelve una promesa
+//     $.get('https://reqres.in/api/users').then( 
 //         (respuesta) => {
 //             console.log('dentro del then');
 //             console.log(respuesta);
 //         });
 //     console.log('después de la petición');
 // });
+
+// Petición con API Fetch, nativa de JS
+// Cuando la página cargue hago la petición
+$( document ).ready(function() {
+    console.log('Antes de la petición');
+    // Hago una petición a una web y devuelve una promesa
+    fetch('https://reqres.in/api/users').then( 
+        (respuesta) => {
+            console.log('dentro del then');
+            console.log(respuesta);
+        });
+    console.log('después de la petición');
+});
